e23a533cd3fee7bbb1fd5e338d8c5c37ec38ee8b
Innocent TIALO <tialoinnocent@gmail.com>
2021-05-02 08:06:25 +0100

    addition of necessary jpa repositories and the data.sql file for base data in the database

---
+++ src/main/java/com/africafuture/recipe/repository/UnitOfMeasureRepository.java
@@ -0,0 +1,9 @@
+package com.africafuture.recipe.repository;
+
+import com.africafuture.recipe.domain.UnitOfMeasure;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure, Long> {
+}
