9778fed1b7090358b1a617dd668a5b337ab516a7
Innocent TIALO <tialoinnocent@gmail.com>
2021-05-07 13:44:53 +0100

    adding Recipe Service test

---
+++ src/test/java/com/africafuture/recipe/service/impl/RecipeServiceImplTest.java
@@ -0,0 +1,43 @@
+package com.africafuture.recipe.service.impl;
+
+import com.africafuture.recipe.domain.Recipe;
+import com.africafuture.recipe.repository.RecipeRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+class RecipeServiceImplTest {
+
+    RecipeServiceImpl recipeService;
+
+    @Mock
+    RecipeRepository recipeRepository;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        recipeService = new RecipeServiceImpl(recipeRepository);
+    }
+
+    @Test
+    void findAll() {
+        Recipe recipe = new Recipe();
+        Set<Recipe> recipeData = new HashSet<>();
+        recipeData.add(recipe);
+        when(recipeRepository.findAll()).thenReturn(recipeData);
+
+        Set<Recipe> recipes = recipeService.findAll();
+
+        verify(recipeRepository, times(1)).findAll();
+        assertEquals(1, recipes.size());
+    }
+}
